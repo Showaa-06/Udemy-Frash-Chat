5faabb648f6f2383619fce46729488f9833b2795
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2023-01-14 14:52:51 +0900

    first commit

---
+++ Flash Chat iOS13/Controllers/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Cintent.swift
+//  Flash Chat iOS13
+//
+//  Created by 鈴木彰悟 on 2023/01/10.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 鈴木彰悟 on 2023/01/12.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
